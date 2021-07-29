34d083cc2739345caecff2b0f0150746250ed43d
Filip <filipmarkovic@gmail.com>
2021-07-29 09:49:27 +0200

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
-import React, { useEffect, useState } from 'react'
-import { BrowserRouter as Router} from 'react-router-dom'
-import Header from './components/Header'
-import Main from './components/Main'
-import Footer from './components/Footer'
+import React, { useEffect, useState } from "react"
+import { BrowserRouter as Router } from "react-router-dom"
+import Header from "./components/Header"
+import Main from "./components/Main"
+import Footer from "./components/Footer"
 
 function App() {
   const [data, setData] = useState([])
-console.log(data)
-    useEffect(() => {
-        fetch('http://localhost:8000/workshops')
-            .then(res => res.json())
-            .then(data => setData(data))
-        
-    }, [])
-    
+  console.log(data)
+  useEffect(() => {
+    fetch("http://localhost:8000/workshops")
+      .then((res) => res.json())
+      .then((data) => setData(data))
+  }, [])
+
   return (
-      <div className="App">        
-          <Router>
-              <Header />
-              <Main data={data} />
-              <Footer />
-          </Router>        
-      </div>    
+    <div className="App">
+      <Router>
+        <Header />
+        <Main data={data} />
+        <Footer />
+      </Router>
+    </div>
   )
 }
 
